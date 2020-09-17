7ea846134598a6cf4d5558cd87ab2ae4096d1094
jzh1 <jzh1@users.noreply.github.com>
2020-09-17 04:19:36 +0000

    Apply fixes from StyleCI

--- src/SendMessage.php
+++ src/SendMessage.php
@@ -132,7 +132,7 @@ class SendMessage
             'msgtype' => 'link',
             'link' => [
                 'title' => $title,
-                'text' =>  "## ".'【'.$this->environmental.'】'."\n>".$sendMessage,
+                'text' => '## '.'【'.$this->environmental.'】'."\n>".$sendMessage,
                 'picUrl' => $picUrl,
                 'messageUrl' => $messageUrl,
             ],
